56fe105e3ad81dd9664ea9129b3f826913b980f2
Washington Reinoso <paulreinosoares463@gmail.com>
2025-08-01 18:01:56 -0500

    clase 22 decoradores

--- 01 typeScriptIntro/src/main.ts
+++ 01 typeScriptIntro/src/main.ts
@@ -1,19 +1,28 @@
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import { name, templateString } from './bases/01-types.ts';
-import { bulbasaur, pokemons } from './bases/02-objects.ts';
-import { charmanderAxios } from './bases/04-injections.ts';
-import { charmanderFetch } from './bases/04-injections.ts';
+// import typescriptLogo from './typescript.svg';
+// import { name, templateString } from './bases/01-types.ts';
+// import { bulbasaur, pokemons } from './bases/02-objects.ts';
+// import { charmanderAxios } from './bases/04-injections.ts';
+// import { charmanderFetch } from './bases/04-injections.ts';
+import { charmander } from './bases/05-decoradores.ts';
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//   <h1>TypeScript + Vite HELLO!!</h1>  
+//   <h1>${name}</h1>    
+//   <h1>${templateString}</h1>  
+//   <h1>${bulbasaur.name}</h1>  
+//   <h1>${charmanderAxios.name}</h1>  
+//   <h1>${charmanderFetch.name}</h1>  
+//   <h1>${charmander.name}</h1>  
+//   <h1>${pokemons.map((pokemon) => pokemon.name).join(', ')}</h1>  
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />      
+//   <div>
+// `;
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   <h1>TypeScript + Vite HELLO!!</h1>  
-  <h1>${name}</h1>    
-  <h1>${templateString}</h1>  
-  <h1>${bulbasaur.name}</h1>  
-  <h1>${charmanderAxios.name}</h1>  
-  <h1>${charmanderFetch.name}</h1>  
-  <h1>${pokemons.map((pokemon) => pokemon.name).join(', ')}</h1>  
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />      
-  <div>
+  <h1>${charmander.screan()}</h1>    
 `;
