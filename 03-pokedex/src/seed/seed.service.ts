6f10e52d4fd1ce2f83550b452cfad86328f4775a
Washington Reinoso <paulreinosoares463@gmail.com>
2025-08-08 17:12:31 -0500

    clase 95 - insertar multiples registros por lote

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/Poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(private readonly pokemonService: PokemonService) {}
+
   async excecuteSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
-      const segments = url.split('/');
-      const no = +segments[segments.length - 2];
-      console.log(no, name);
-    });
+    try {
+      data.results.forEach(({ name, url }) => {
+        const segments = url.split('/');
+        const no = +segments[segments.length - 2];
 
-    return data.results;
+        this.pokemonService.create({ no, name });
+      });
+      return data.results;
+    } catch (error) {
+      throw new NotFoundException(`can't insert for lote: ${error}`);
+    }
   }
 }
